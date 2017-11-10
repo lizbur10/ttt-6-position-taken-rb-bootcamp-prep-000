af3a00d1789235ea27c533f25715da561b793de2
Liz Burton <liz.burton147@gmail.com>
2017-11-10 14:45:34 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,9 @@
 # code your #position_taken? method here!
-def position_taken?(board, index)
-  position =   index.to_i - 1
-
+def position_taken?(board, position)
+  index = position.to_i - 1
+  if board[index] == " "
+    return true
+  else 
+    return false
+  end
 end
